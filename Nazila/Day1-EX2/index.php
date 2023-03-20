166f66825c238d3df88de015fbb9c561cbd5cc82
Nazila <azabdaftar@outlook.com>
2023-03-20 11:14:51 +0100

    Ex2

---
+++ Nazila/Day1-EX2/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="../Css/style.css">
+    <title>PHP-Day1</title>
+</head>
+<body>
+    <div class="containerex2">
+        <?php
+        $fName= "Nazila";
+        $age=40;
+        $jobTitle="Computer engineer";
+        echo "Hi, my Name is"." ".$fName.",and i am"." ".$age." "."and i work as a"." ".$jobTitle;
+        ?>
+    </div>
+</body>
+</html>
